b00f4443c3a7920f346273cc5112c74275bee076
20120082 <115605479+20120082@users.noreply.github.com>
2022-10-15 20:15:52 +0700

    Update help.c

--- code/test/help.c
+++ code/test/help.c
@@ -11,7 +11,7 @@ int main() {
     PrintString("\t||  20120438\t||    Dao Van Canh   ||\n");
     PrintString("\t'====================================='\n");
     PrintString("\n>ASCII: at directory code, run build.linux/nachos -x test/ascii to print the ASCII table\n");
-    PrintString(">Sort: \tat directory code, run build.linux/nachos -x test/bubblesort to start the sort program\n");
+    PrintString(">Sort: \tat directory code, run build.linux/nachos -x test/bubble_sort to start the sort program\n");
     PrintString("\t- Enter n (the length of the array, 0 <= n <= 100)\n");
     PrintString("\t- Enter n elements of the array\n");
     PrintString("\t- Enter the order you want to sort the array with (1: increasing, 2: decreasing)\n");
