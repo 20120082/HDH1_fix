fbb38fd5077418e2f67b17638034004552eb945c
20120082 <115605479+20120082@users.noreply.github.com>
2022-11-20 09:40:08 +0700

    Update delete.c

--- code/test/delete.c
+++ code/test/delete.c
@@ -3,11 +3,18 @@
 int main() {
     char fileName[256];
     int size;
+    int success;
 
     PrintString("nhap so luong ki tu co trong file name (co tinh .txt): ");
     size=ReadNum();
     PrintString("nhap ten file : ");
     ReadString(fileName, size);
-    Remove(fileName);
+    success=Remove(fileName);
+
+    if(success==1) 
+    PrintString("Xoa file thanh cong");
+    else
+    PrintString("Xoa file that bai");
+    Halt();
 
 }
