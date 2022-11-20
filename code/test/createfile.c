a9c9b788df767aa1dab39dd9730f1ce27705ad83
20120082 <115605479+20120082@users.noreply.github.com>
2022-11-20 09:11:02 +0700

    Create createfile.c

---
+++ code/test/createfile.c
@@ -0,0 +1,13 @@
+#include "syscall.h"
+
+int main() {
+    char* fileName ="hello.txt";
+
+
+    if (Create(fileName) == 0) {
+        PrintString("File ");
+        PrintString(fileName);
+        PrintString(" da tao thanh cong\n");
+    } else
+        PrintString("Tao file that bai!\n");
+}
