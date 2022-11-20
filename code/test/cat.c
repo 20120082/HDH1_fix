f1f73b8fb141fb2da284fbe561f4bad4a95e08a6
20120082 <115605479+20120082@users.noreply.github.com>
2022-11-20 13:17:55 +0700

    Update cat.c

--- code/test/cat.c
+++ code/test/cat.c
@@ -1,15 +1,12 @@
 #include "syscall.h"
 
 #define MODE_READWRITE 0
-#define MODE_WRITE 1
 
-#define stdin 0
-#define stdout 1
 
 int main()
 {
     char str[100];
-    int id = Open("hello.txt", 0);
+    int id = Open("hello.txt", MODE_READWRITE);
     Read(str, 50, id);
     PrintString(str);
     PrintString("\n");
